e6572c839db8017aec53c82aeac9f1a9b5f3ff07
Joel Jose <jjose@bljzbw.local>
2017-03-03 15:22:10 +0100

    stuff

---
+++ php/poll.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title>Poll</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../css/normalize.css" rel="stylesheet">
+        <link rel="stylesheet" href="../css/style.css">
+        <?php 
+        
+        ?>
+    </head>
+    <body id="poll">
+        <section id="content">
+
+                <div class="flex" id="mainflex">
+                        
+                        <aside id="aside" class="flex">
+                            <div id="list">
+                            <ul>
+                                <li><h2><a href="index.html">&nbsp;&nbsp;Home</a></h2></li>
+                                <li><h2><a href="#">&nbsp;&nbsp;Discussion</a></h2></li>
+                                <li><h2><a href="#">&nbsp;&nbsp;Gallery</a></h2></li>
+                                <li><h2><a href="#">&nbsp;&nbsp;About</a></h2></li>
+                                <li><h2><a href="../php/community.php">&nbsp;&nbsp;Community</a></h2></li>
+                                <li><h2><a href="../html/poll.html">&nbsp;&nbsp;Poll</a></h2></li>
+                            </ul>
+                            </div>
+                            <div id="spacer">
+                            </div>
+                            <div id="socialmedia">
+                               <i class="fa fa-facebook-official" aria-hidden="true"></i>
+                                <i class="fa fa-google-plus-square" aria-hidden="true"></i>
+                                <i class="fa fa-twitter" aria-hidden="true"></i>
+                                     <i class="fa fa-instagram" aria-hidden="true"></i>
+                                     
+                             
+                            </div>
+                            <div id="copyright">
+                                <h4>Copyright</h4>
+                                
+                                    All images and content belong to me, Joel Jose.
+                                
+                            </div>
+                        </aside>
+
+                    
+                         <div>
+                             
+                         </div>
+
+                               
+                </div>
+                </div>
+        </section>
+          
+    </body>
+</html>
+
+
+
+
