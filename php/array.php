738ba98da5c6ae5b9391c68c093f0b68fdcf0fb6
Joel Jose <jjose@bljzbw.local>
2017-03-08 16:57:49 +0100

    adfs

--- php/array.php
+++ php/array.php
@@ -1,19 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="../css/array.css" rel="stylesheet">
+    </head>
+    <body>
+   
+  
 <?php
+        $vorname=$_POST['vorname'];
+        $nachname=$_POST['nachname'];
+        $handy=$_POST['handy'];
+        $telefon=$_POST['telefon'];
+
 
         $array1 = array(
-                            array('Hans', 'Muster'),
-                            array('Peter','Test'),
-                            array('Ben','Tester'),
-                            array('Leon','Testest'),
-                            array('Johanna','Tess'),
+                            array('Hans', 'Muster', 'handy' => '079 851 13 57', 'telefon' => '071 985 04 19'),
+                            array('Peter','Test','handy' => '079 851 13 57', 'telefon' => '071 985 04 19'),
+                            array('Ben','Tester','handy' => '079 851 13 57', 'telefon' => '071 985 04 19'),
+                            array('Leon','Testest','handy' => '079 851 13 57', 'telefon' => '071 985 04 19'),
+                            array('Johanna','Tess','handy' => '079 851 13 57', 'telefon' => '071 985 04 19'),
+                            array()
                         );
 
-        $handy = []
-                        
+
+
+              
 
                         echo "<table>";
                         echo "<tr>";
-                        echo "<th>Vorname</th><th>Nachname</th>";
+                        echo "<th>Vorname</th><th>Nachname</th><th>Handy</th><th>Telefon</th>";
                         echo "</tr>";
 
                         foreach($array1 as $temp) {
@@ -21,10 +39,12 @@
 
                                     foreach($temp as $temp2){
                                             
-                                            echo "<td>"; 
+                                            echo "<td>";
                                             echo $temp2. " ";
                                             echo "</td>";
-                                            } 
+                                             
+                                            }
+                                            
                                
                             echo "</tr>";
  
@@ -32,4 +52,17 @@
                         echo "</table>";
 
 
-?>
+?>  
+
+ <form action="array.php" method="post">
+        <input type="text" name="vorname"> Vorname<br><br><br><br>
+        <input type="text" name="nachname"> nachname<br><br><br><br>
+        <input type="number" name="handy"> Handy<br><br><br><br>
+        <input type="number" name="telefon"> Telefon<br><br><br><br>
+        <input type="submit"value="hinzufügen">
+        <?php 
+        $array1[][$vorname, $nachname, 'handy'=> $handy, 'telefon' => $telefon];
+        ?>
+    </form>
+</body>
+</html>
